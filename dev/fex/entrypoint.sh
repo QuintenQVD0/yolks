f6fc0b5a38ad87c4cf4668e0c1e0ab6ed80d5bd9
Quinten <67589015+QuintenQVD0@users.noreply.github.com>
2023-03-25 13:10:26 +0100

    Finish FEX

--- dev/fex/entrypoint.sh
+++ dev/fex/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/container
+cd /home/container || exit
 
 # Make internal Docker IP address available to processes.
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
@@ -8,6 +8,27 @@ export INTERNAL_IP
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+
+if [ -d "/home/container/rootfs" ]
+then
+    echo -e "\nThis server will need at least 9GB of disk space!"
+    export FEX_APP_DATA_LOCATION=/home/container/rootfs/; export FEX_APP_CONFIG_LOCATION=/home/container/; export XDG_DATA_HOME=/home/container; FEXRootFSFetcher -y -x --distro-name=ubuntu --distro-version=22.04
+else
+    echo "RootFS already downloaded"
+fi
+
+sleep 2
+
+if [ -f "/home/container/Config.yml" ]; then
+    echo -e "\nNeeded config file exists, skipping"
+else 
+    echo -e "\nNeeded config file does not exist. exiting"
+    exit 0
+fi
+
+export FEX_APP_DATA_LOCATION=/home/container/rootfs/
+export FEX_APP_CONFIG_LOCATION=/home/container/
+export XDG_DATA_HOME=/home/container
 
 # Run the Server
 eval ${MODIFIED_STARTUP}
