c21b8d4397d75c214c17c86b2f1335299425f9af
Quinten <67589015+QuintenQVD0@users.noreply.github.com>
2023-04-09 15:48:15 +0200

    back to eval

--- temp/rust/entrypoint.sh
+++ temp/rust/entrypoint.sh
@@ -30,4 +30,4 @@ MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-exec ${MODIFIED_STARTUP}
+eval ${MODIFIED_STARTUP}
