605c9c5e51d5d6d1990706fba8f3b36e73bd513c
Quinten <67589015+QuintenQVD0@users.noreply.github.com>
2024-01-28 16:44:18 +0100

    Try to setup auto update with fex

--- dev/fex/entrypoint.sh
+++ dev/fex/entrypoint.sh
@@ -19,17 +19,48 @@ fi
 
 sleep 2
 
+export FEX_APP_DATA_LOCATION=/home/container/rootfs/
+export FEX_APP_CONFIG_LOCATION=/home/container/
+export XDG_DATA_HOME=/home/container
+
 if [ -f "/home/container/Config.json" ]; then
     echo -e "\nNeeded config file exists, skipping"
+    # Switch to the container's working directory
+    cd /home/container || exit 1
+
+    ## just in case someone removed the defaults.
+    if [ "${STEAM_USER}" == "" ]; then
+        echo -e "steam user is not set.\n"
+        echo -e "Using anonymous user.\n"
+        STEAM_USER=anonymous
+        STEAM_PASS=""
+        STEAM_AUTH=""
+    else
+        echo -e "user set to ${STEAM_USER}"
+    fi
+
+    ## if auto_update is not set or to 1 update
+    if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then 
+        # Update Source Server
+        if [ ! -z ${SRCDS_APPID} ]; then
+            if [ "${STEAM_USER}" == "anonymous" ]; then
+                export HOME=/home/container; FEXInterpreter ./steamcmd/steamcmd.sh +force_install_dir /home/container +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH} $( [[ "${WINDOWS_INSTALL}" == "1" ]] && printf %s '+@sSteamCmdForcePlatformType windows' ) +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${HLDS_GAME} ]] || printf %s "+app_set_config 90 mod ${HLDS_GAME}" )  ${INSTALL_FLAGS} $( [[ "${VALIDATE}" == "1" ]] && printf %s 'validate' ) +quit
+            else
+                export HOME=/home/container; FEXInterpreter numactl --physcpubind=+0 ./steamcmd/steamcmd.sh +force_install_dir /home/container +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH} $( [[ "${WINDOWS_INSTALL}" == "1" ]] && printf %s '+@sSteamCmdForcePlatformType windows' ) +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${HLDS_GAME} ]] || printf %s "+app_set_config 90 mod ${HLDS_GAME}" ) ${INSTALL_FLAGS} $( [[ "${VALIDATE}" == "1" ]] && printf %s 'validate' ) +quit
+            fi
+        else
+            echo -e "No appid set. Starting Server"
+        fi
+
+    else
+        echo -e "Not updating game server as auto update was set to 0. Starting Server"
+    fi
+
 else 
     echo -e "\nNeeded config file does not exist. exiting"
     exit 0
 fi
 
-
-export FEX_APP_DATA_LOCATION=/home/container/rootfs/
-export FEX_APP_CONFIG_LOCATION=/home/container/
-export XDG_DATA_HOME=/home/container
 
 # Run the Server
 eval ${MODIFIED_STARTUP}
