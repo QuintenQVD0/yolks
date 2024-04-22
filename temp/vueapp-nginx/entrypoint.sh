9f9ddf8643479c54c0bbbef9b8dab9f9748a430e
Quinten <67589015+QuintenQVD0@users.noreply.github.com>
2024-04-22 20:10:42 +0200

    add vueapp-nginx

---
+++ temp/vueapp-nginx/entrypoint.sh
@@ -0,0 +1,19 @@
+@ -0,0 +1,18 @@
+#!/bin/bash
+cd /home/container
+
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Print Node.js Version
+node -v
+
+rm -rf /home/container/.nginx/tmp/*
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+echo "⟳ Starting Nginx..."
+# Run the Server
+eval ${MODIFIED_STARTUP}
